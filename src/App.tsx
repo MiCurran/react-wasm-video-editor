c6c103aa82756e2c333c0c2609098f880183a107
Micurran <mcurran1722@gmail.com>
2023-07-22 17:12:49 -0600

    clean up styles - add reset button

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,15 @@ function App() {
   const [ready, setReady] = useState(false);
   const [video, setVideo] = useState<any>();
   const [trimmed, setTrimmed] = useState<string>();
-  const [previewVideo, setPreviewVideo] = useState<any>(video);
-  const [loading, setLoading] = useState<boolean>(false);
 
   const load = async () => {
     await ffmpeg.load();
     setReady(true);
+  }
+
+  const reset = () => {
+    setTrimmed(undefined);
+    setVideo(undefined);
   }
 
   useEffect(() => {
@@ -24,11 +27,7 @@ function App() {
     setReady(false)
     // Write the file to memory 
     ffmpeg.FS('writeFile', 'test.mp4', await fetchFile(video));
-
     await ffmpeg.run('-i', 'test.mp4', '-ss', '00:00:00', '-to', '00:00:10', '-f', 'mp4', 'output.mp4')
-    //To GIF
-    //await ffmpeg.run('-i', 'test.mp4', '-t', '2.5', '-ss', '2.0', '-f', 'gif', 'out.gif');
-
     // Read the result
     const data = ffmpeg.FS('readFile', 'output.mp4');
 
@@ -41,23 +40,37 @@ function App() {
   return ready ? (
     
     <div className="App">
-      { video && !trimmed && 
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+        }}
+      >
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column'
+          }}
+        >
+          { video && !trimmed && 
+          <>
           <video
             controls
             width="250"
             src={URL.createObjectURL(video)}>
           </video>
-      }
-
-
-      <input type="file" onChange={(e) => setVideo(e.target.files?.item(0))} />
-
-      <h3>Result</h3>
-
-      <button onClick={convertToGif}>Trim</button>
-
-      { trimmed && <video controls src={trimmed} width="250" />}
-
+          <button onClick={convertToGif}>Trim</button>
+          </>
+          }
+          { trimmed && 
+            <>
+              <video controls src={trimmed} width="250" />
+              <button onClick={reset}>reset</button>
+            </>
+          }
+          <input type="file" onChange={(e) => setVideo(e.target.files?.item(0))} />
+        </div>
+      </div>
     </div>
   )
     :
